56fd91a05bdb7574eaeb6dd15d1e269ed82c8871
Joe Renteria <joe@electronflight.com>
2021-07-21 16:43:31 -0700

    commit tree 64ebc5898e482a14f95e751eb3e1ca251b85b430

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if(distance<=400){
+    console.log(`This one is on me!`);
+    return `This one is on me!`;
+    
+  }
+
+  else if(distance>2500){
+    return `No can do.`;
+    
+  }
+
+  else if(distance>2000){
+    return `I will gladly take your thirty bucks.`;
+    
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === `NYC` ? `Ok, sounds good.` : `No go.`);
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+if(tip === `generous`){
+  return `Thank you so much.`;
+}
+
+else if(tip === `not as generous`){
+  return `Thank you.`;
+}
+
+else{
+  return `Bye.`;
+}
+
 }
